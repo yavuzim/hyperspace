5cb97d06f73939f11945d1e6ebc82b578ca9f417
yavuzim <kyavuzimer@gmail.com>
2024-11-14 09:55:39 +0300

    created_services

---
+++ hyperspace/services/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Service
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_featured', 'slug')
+    search_fields = ('title', 'description')
+    list_filter = ('is_featured',)
